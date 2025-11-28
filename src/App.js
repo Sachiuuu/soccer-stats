71180ec7fb393b477532388990b7497355067de0
Daniel Diaz Forero <d_diazforero@fanshaweonline.ca>
2025-11-28 18:45:14 -0500

    Deleted python enviroment due to problems to fetch all data for the lineups. To replace this, now there will be and extra page that will allow us to save player by player manually.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/club/:id" element={<ClubPage />} />
+          <Route path="/squad-data" element={<SquadDataEntry />} />
         </Routes>
       </div>
     </Router>
